d4fec3fb35946c7cde6dee830172ba9b09efec0d
Leroy Hopson <git@leroy.geek.nz>
2021-11-07 15:38:20 +0700

    Update TODO Manager plugin

--- plug.gd
+++ plug.gd
@@ -6,4 +6,4 @@ extends "res://addons/gd-plug/plug.gd"
 func _plugging():
 	plug("bitwes/Gut", {commit = "70c08aebb318529fc7d3b07f7282b145f7512dee"})
 	plug("lihop/godot-xterm-dist", {commit = "6534aa3379ef09eca70a3e42539e47fe31ce07e4"})
-	plug("lihop/TODO_Manager", {commit = "9c6892e1b7f57d2864794b184fc13338b963d9c2"})
+	plug("OrigamiDev-Pete/TODO_Manager", {commit = "1ce9de52e657d4068348af87982bbb99666626aa"})
