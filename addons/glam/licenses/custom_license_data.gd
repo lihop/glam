014b50fca614b14f797ed0793e74c69a83f83306
Leroy Hopson <git@leroy.geek.nz>
2021-11-20 21:01:19 +0700

    Add Creative Commons Sampling Plus 1.0 license
    License is retired but used by some older freesound.org works.

--- addons/glam/licenses/custom_license_data.gd
+++ addons/glam/licenses/custom_license_data.gd
@@ -6,4 +6,9 @@ const data = {
 		name = "Pixabay License",
 		url = "https://pixabay.com/service/terms/#license",
 	},
+	"LicenseRef-CC-Sampling-Plus-1.0":
+	{
+		name = "Creative Commons Sampling Plus 1.0",
+		url = "https://creativecommons.org/licenses/sampling+/1.0/",
+	},
 }
