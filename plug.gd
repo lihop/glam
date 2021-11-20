24b8a18b174a3254348a20b557aad59268f7f8bd
Leroy Hopson <git@leroy.geek.nz>
2021-11-20 21:05:19 +0700

    Add epic-anchors plugin

--- plug.gd
+++ plug.gd
@@ -5,6 +5,7 @@ extends "res://addons/gd-plug/plug.gd"
 
 func _plugging():
 	plug("bitwes/Gut", {commit = "70c08aebb318529fc7d3b07f7282b145f7512dee"})
+	plug("lihop/godot-epic-anchors", {commit = "3e04f4f30c69caf66ec29299d0a3ecede61043fc"})
 	plug("lihop/godot-xterm-dist", {commit = "6534aa3379ef09eca70a3e42539e47fe31ce07e4"})
 	plug("OrigamiDev-Pete/TODO_Manager", {commit = "1ce9de52e657d4068348af87982bbb99666626aa"})
 	plug(
