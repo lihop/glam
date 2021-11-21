c9d6f6444f4313f087e6c6f47f1c35823c0f038a
Leroy Hopson <git@leroy.geek.nz>
2021-11-21 11:33:33 +0700

    Revert "Fix thumbnail grid error"
    This reverts commit 9f735611a5b9576445280d638add5487a6e5986d.

--- addons/glam/controls/thumbnail_grid/thumbnail_grid.gd
+++ addons/glam/controls/thumbnail_grid/thumbnail_grid.gd
@@ -32,7 +32,7 @@ func clear() -> void:
 
 
 func append(assets := []) -> void:
-	var first := true
+	var first := get_child_count() == 0
 	for asset in assets:
 		assert(asset is Asset, "%s is not an Asset." % asset)
 		var thumbnail: Thumbnail = ThumbnailScene.instance()
@@ -41,9 +41,8 @@ func append(assets := []) -> void:
 		thumbnail.connect("download_requested", self, "_on_download_requested")
 		add_child(thumbnail)
 		thumbnail.asset = asset
-		if first:
-			thumbnail.pressed = true
-			first = false
+	if first:
+		get_child(0).pressed = true
 
 
 func _on_thumbnail_toggled(pressed: bool, thumbnail: Thumbnail) -> void:
