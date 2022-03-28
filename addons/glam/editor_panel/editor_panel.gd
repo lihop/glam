0e3aa12fe2141267544ed77bb63a94b7c1da0c4f
Leroy Hopson <git@leroy.geek.nz>
2022-03-28 22:58:58 +0700

    Add ability to generate CREDITS.md
    Generates a CREDITS.md markdown file from existing asset metadata.
    Closes #6.

--- addons/glam/editor_panel/editor_panel.gd
+++ addons/glam/editor_panel/editor_panel.gd
@@ -3,6 +3,9 @@
 tool
 extends Control
 
+const FileScanner := preload("../util/file_scanner.gd")
+const Markdown := preload("../credits/markdown.gd")
+
 signal source_changed(new_source)
 
 var sources := []
@@ -10,6 +13,7 @@ var sources := []
 onready var source_panels := $VBoxContainer/SourcePanels
 onready var source_select: OptionButton = $VBoxContainer/HBoxContainer/SourceSelect
 onready var _query_bar := find_node("QueryBar")
+onready var _menu_button := find_node("MenuButton")
 
 
 func _ready():
@@ -39,6 +43,12 @@ func _ready():
 	for source in sources:
 		source.panel.show()
 
+	_menu_button.get_popup().clear()
+	_menu_button.get_popup().add_item("Generate Licenses", 0)
+	#_menu_button.get_popup().add_item("Generate credits.json", 1)
+	_menu_button.get_popup().add_item("Generate CREDITS.md", 2)
+	_menu_button.get_popup().connect("id_pressed", self, "_on_menu_id_pressed")
+
 	select_source(0)
 
 
@@ -60,3 +70,64 @@ func select_source(index: int):
 	if source:
 		_query_bar.source = source
 		emit_signal("source_changed", source)
+
+
+func _on_menu_id_pressed(id: int):
+	var paths := _get_asset_paths_rec()
+
+	match id:
+		0:  # Generate Licenses.
+			var dir := Directory.new()
+			for path in paths:
+				var asset: GLAMAsset = load(path)
+				if dir.file_exists(path.get_basename()):
+					asset.create_license_file(path.get_basename())
+
+		1:  # Generate credits.json.
+			var file := File.new()
+
+			if file.open("credits.json", File.WRITE) != OK:
+				return
+
+			file.store_line(
+				"""{
+	"__comment": "File generate by GLAM. Do not modify! Edit the '.glam' files throughout the project instead.",
+	"credits": ["""
+			)
+
+			for i in paths.size():
+				var path = paths[i]
+				var asset: GLAMAsset = load(path)
+				file.store_line('		"%s"%s' % [path, "," if i < (paths.size() - 1) else ""])
+
+			file.store_line(
+				"""	]
+}
+"""
+			)
+			file.close()
+
+		2:  # Generate CREDITS.md.
+			var file := File.new()
+			assert(
+				file.open("res://CREDITS.md", File.WRITE) == OK,
+				"Couldn't open res://CREDITS.md file for writing."
+			)
+			file.store_string(Markdown.generate_credits("res://", self.sources))
+			file.close()
+
+
+static func _get_asset_paths_rec(root := "res://") -> Array:
+	var paths := []
+	var file := File.new()
+
+	for path in FileScanner.list_files_rec(root):
+		if path is String:
+			var asset: GLAMAsset = load(path)
+
+			if asset == null:
+				continue
+
+			paths.append(path)
+
+	return paths
